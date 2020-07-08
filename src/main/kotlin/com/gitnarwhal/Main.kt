f9ac0a182a28e1031ac5680ea639345bf7e8123d
Celeste Capece <celedev97@gmail.com>
2020-07-08 11:44:01 +0200

    Added helper classes for loading fxml of components and views Closes #9

--- src/main/kotlin/com/gitnarwhal/Main.kt
+++ src/main/kotlin/com/gitnarwhal/Main.kt
@@ -2,43 +2,24 @@ package com.gitnarwhal
 
 import com.gitnarwhal.views.MainView
 import javafx.application.Application
-import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.scene.image.Image
 import javafx.stage.Stage
-import java.util.*
+
 import java.util.jar.Manifest
 
-
 fun main(){
-    var manifestAttributes = Manifest(GitNarwhal::class.java.classLoader.getResource("META-INF/MANIFEST.MF").openStream()).getMainAttributes();
+    var manifestAttributes = Manifest(GitNarwhal::class.java.classLoader.getResource("META-INF/MANIFEST.MF").openStream()).mainAttributes;
     var version = manifestAttributes.getValue("Specification-Version")
     println("RUNNING GITNARWHAL "+version)
     Application.launch(GitNarwhal::class.java)
 }
 
 class GitNarwhal() : Application() {
-
     override fun start(primaryStage: Stage) {
-        primaryStage.scene = Scene(fxmlView<Parent>("MainView"))
+        primaryStage.scene = Scene(MainView().root)
         primaryStage.icons.add(Image(GitNarwhal::class.java.getResourceAsStream("/icon.png")));
         primaryStage.show();
-    }
-
-    companion object{
-        //region fxml helpers
-        fun <T> fxmlView(url: String = "", clazz: Class<*>? = null): T {
-            return fxml("/views/" + (clazz?.simpleName ?: url) + ".fxml")
-        }
-
-        fun <T> fxmlComponent(url: String = "", clazz: Class<*>? = null): T {
-            return fxml("/components/" + (clazz?.simpleName ?: url) + ".fxml")
-        }
-
-        fun <T> fxml(absoluteUrl: String): T {
-            return FXMLLoader.load<T>(GitNarwhal::class.java.getResource(absoluteUrl))
-        }
-        //endregion
     }
 }
