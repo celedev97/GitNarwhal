137c8876804adb77ca3820fc2c5d64f89a1f0e0c
Fede Capece <capece.1744999@studenti.uniroma1.it>
2020-07-11 23:05:14 +0200

    Ordered backend, git log still not ready
    I'm probably gonna switch to tornadoFX soon, so I won't bother too much making the TableView work for now

--- src/main/kotlin/com/gitnarwhal/utils/Command.kt
+++ src/main/kotlin/com/gitnarwhal/utils/Command.kt
@@ -1,22 +1,41 @@
 package com.gitnarwhal.utils
 
-import java.io.InputStream
+import java.nio.file.Path
 
-class Command(val command: String) {
+class Command(vararg command: String, path: String = "./") {
     var output: String = "";
-    var error: String = "";
 
-    fun execute(): Boolean {
+    var success:Boolean = false;
+    var code:Int = -1;
+
+    val workingDir = Path.of(path).toAbsolutePath().toFile()
+
+    private val command = run {
+        var output = command.toList()
+        if(command.size == 1){
+            output = command[0].split(" ")
+        }
+        output
+    }
+
+    fun execute(): Command {
         //executing command
-        val process = Runtime.getRuntime().exec(command)
-        process.waitFor()
+        val process = ProcessBuilder(command).directory(workingDir).redirectErrorStream(true).start()
 
         //reading result streams
-        output  = String(process.inputStream.readAllBytes())
-        error   = String(process.errorStream.readAllBytes())
+        output = String(process.inputStream.readAllBytes()).trim()
 
-        //returning true if there was no error
-        return process.exitValue() == 0
+        //waiting for process end
+        process.waitFor()
+
+        //getting the exit code
+        code = process.exitValue()
+        success = code == 0
+        return this
+    }
+
+    override fun toString(): String {
+        return command.joinToString (" ")
     }
 
 }
