c21b33183f8286d738a2dc944ab5b9a888861126
Celeste Capece <celedev97@gmail.com>
2020-07-11 11:00:17 +0200

    Settings now works
    fixed an error

--- src/main/kotlin/com/gitnarwhal/utils/Settings.kt
+++ src/main/kotlin/com/gitnarwhal/utils/Settings.kt
@@ -3,37 +3,41 @@ package com.gitnarwhal.utils
 import org.json.JSONObject
 import java.io.File
 import java.io.FileWriter
-import java.io.Writer
 import java.nio.file.Files
 import java.nio.file.Path
 import kotlin.reflect.KProperty
+import kotlin.reflect.full.memberProperties
 
 
 private const val CONFIG = "./gitnarwhal.json"
+private val CONFIG = System.getProperty("user.home") + "/.gitnarwhal.json"
 private val settingsJSON = if(File(CONFIG).exists()) Files.readString(Path.of(CONFIG)) else "{}";
 
 object Settings : JSONObject(settingsJSON) {
-    const val FILE = CONFIG;
+    val FILE = CONFIG;
 
     var autoUpdate by JSON(true)
     var theme by JSON("jar://stylesheets/main.css")
 
     fun write() {
+        //forcing a read on every declared property, this should set the default value for everything that doesn't have a value
+        Settings.javaClass.kotlin.memberProperties.forEach{
+            var test = it.get(Settings)
+            println(test)
+        }
+
         with(FileWriter(FILE)){
-            super.write(FileWriter(FILE))
+            super.write(this, 4,0)
             this.close()
         }
     }
 }
 
 
-class JSON<T> (val default: T) {
-    init {
-        Settings.put(property.name, this.default)
-    }
-
+class JSON<T> (private val default: T) {
     operator fun setValue(settings: Settings, property: KProperty<*>, value: T) {
         settings.put(property.name, value)
+        Settings.write()
     }
 
     operator fun getValue(settings: Settings, property: KProperty<*>): T {
