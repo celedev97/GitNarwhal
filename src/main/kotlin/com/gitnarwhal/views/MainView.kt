ebe00b36b58556631030c8e3e644bc387cc8d8c4
Fede Capece <capece.1744999@studenti.uniroma1.it>
2020-07-13 16:19:50 +0200

    now we're using tornadoFX: +Views and Components now are tornadoFX views and components -removed old implementation for fxml delegate and views/components ~moved file structure to have fxml automatically loaded without the need for passing the path -downgraded jdk to 11 to make it (hopefully) more compatible with tornadoFX... (it's only compatible with java 8 for now)

--- src/main/kotlin/com/gitnarwhal/views/MainView.kt
+++ src/main/kotlin/com/gitnarwhal/views/MainView.kt
@@ -4,18 +4,18 @@ package com.gitnarwhal.views
 import com.gitnarwhal.components.RepoTab
 import javafx.fxml.FXML
 import javafx.scene.Parent
+import javafx.scene.control.Tab
 import javafx.scene.control.TabPane
+import tornadofx.*
 import java.net.URL
 import java.util.*
 
-class MainView : View {
-    val root:Parent = View.fxml(this);
+class MainView : View() {
+    override val root:Parent by fxml(null as String?, true)
+    val tabPane :TabPane by fxid()
 
-    @FXML lateinit var tabPane : TabPane
-
-
-    override fun initialize(location: URL?, resources: ResourceBundle?) {
-        tabPane.tabs.add(RepoTab("./"))
+    init {
+        tabPane.tabs.add(RepoTab("./").tab)
     }
 
 }
