bdc9d09afebf96a9e1f83e736868445c65657c00
Fede Capece <capece.1744999@studenti.uniroma1.it>
2020-07-13 18:25:07 +0200

    git log first version
    it needs a lot of polishing, it still can't properly extract the title from commits and the graph is not present at all

--- src/main/kotlin/com/gitnarwhal/backend/Git.kt
+++ src/main/kotlin/com/gitnarwhal/backend/Git.kt
@@ -7,7 +7,7 @@ class Git(val repo: String) {
     companion object{
         val GIT:String = GitDownloader.GIT
         const val FAKE_SEP = "g\\nrwl?/>"
-        const val FORMAT = "{${FAKE_SEP}commit$FAKE_SEP:$FAKE_SEP%H$FAKE_SEP,${FAKE_SEP}author$FAKE_SEP:$FAKE_SEP%aN<%aE>$FAKE_SEP,${FAKE_SEP}date$FAKE_SEP:$FAKE_SEP%ad$FAKE_SEP,${FAKE_SEP}message$FAKE_SEP:$FAKE_SEP%f$FAKE_SEP}"
+        const val FORMAT = "{${FAKE_SEP}commit${FAKE_SEP}:${FAKE_SEP}%H${FAKE_SEP},${FAKE_SEP}author${FAKE_SEP}:${FAKE_SEP}%aN<%aE>${FAKE_SEP},${FAKE_SEP}date${FAKE_SEP}:${FAKE_SEP}%ad${FAKE_SEP},${FAKE_SEP}description${FAKE_SEP}:${FAKE_SEP}%f${FAKE_SEP}}"
     }
 
     fun status() = Command("$GIT status").execute()
