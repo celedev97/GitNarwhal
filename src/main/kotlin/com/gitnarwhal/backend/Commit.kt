857dd4e5cb1ccc4f1e459a12428706a1379bdd39
Celeste Capece <celedev97@gmail.com>
2020-07-16 16:27:36 +0200

    Switch branches
    Still unrefined, doesn't give any message if there are conflicts that prevent switching the branch

--- src/main/kotlin/com/gitnarwhal/backend/Commit.kt
+++ src/main/kotlin/com/gitnarwhal/backend/Commit.kt
@@ -1,5 +1,6 @@
 package com.gitnarwhal.backend
 
+import com.gitnarwhal.views.RepoTab
 import javafx.beans.property.SimpleStringProperty
 import javafx.beans.property.StringProperty
 import javafx.scene.Node
@@ -10,18 +11,50 @@ import org.json.JSONObject
 import tornadofx.View
 import tornadofx.add
 import tornadofx.hbox
+import java.lang.Exception
+import kotlin.reflect.KProperty
 
-class Commit(json:JSONObject, graph:String){
-    var title       :String = json.getString("description").lines().first()
-    var date        :String = json.getString("date")
-    var author      :String = json.getString("author")
-    var commit      :String = json.getString("commit")
+class Commit(hash:String, val repoTab: RepoTab){
+    var hash      :String = hash
 
-    var graph       :Node = {
+    val show = GitShow()
+
+    var title by show
+    var date by show
+    var author by show
+
+    val childs = arrayListOf<Commit>()
+    val parents = arrayListOf<Commit>()
+
+    var graph = run {
         val node = HBox()
         for (i in 1..3){
             node.add(Circle(10.0, Color.gray(.0)))
         }
         node
-    }.invoke()
+    }
+
+    var data: List<String>? = null
+}
+
+open class GitShow(){
+    operator fun getValue(commit: Commit, property: KProperty<*>): Any {
+        if(commit.data == null)
+            with(commit.repoTab.git.show(commit)){
+                if (!success)
+                    throw Exception("can't get data for commit: ${commit.hash}")
+                commit.data = output.lines()
+            }
+
+        return when(property.name) {
+            "title" -> commit.data!![4].trim()
+            "date" -> commit.data!![2].substringAfter(':').trim()
+            "author" -> commit.data!![1].substringAfter(':').trim()
+            else -> throw Exception("Don't know how to parse property: ${property.name}")
+        }
+    }
+
+    operator fun setValue(commit: Commit, property: KProperty<*>, any: Any) {
+        TODO("Not yet implemented")
+    }
 }
--- src/main/kotlin/com/gitnarwhal/backend/Git.kt
+++ src/main/kotlin/com/gitnarwhal/backend/Git.kt
@@ -10,17 +10,34 @@ class Git(val repo: String) {
         const val FORMAT = "{${FAKE_SEP}commit${FAKE_SEP}:${FAKE_SEP}%H${FAKE_SEP},${FAKE_SEP}author${FAKE_SEP}:${FAKE_SEP}%aN<%aE>${FAKE_SEP},${FAKE_SEP}date${FAKE_SEP}:${FAKE_SEP}%ad${FAKE_SEP},${FAKE_SEP}description${FAKE_SEP}:${FAKE_SEP}%f${FAKE_SEP}}"
     }
 
-    fun status() = Command("$GIT status").execute()
+    private fun git(vararg command:String, prependGit: Boolean = true) : Command{
+        var realCommand = command
+        if(prependGit){
+            val list = realCommand.toMutableList()
+            list.add(0,GIT)
+            realCommand = list.toTypedArray()
+        }
 
-    fun log():Command {
-        val out = Command("$GIT --no-pager log --graph --pretty=format:'$FORMAT'").execute()
-        out.output = out.output.replace("\"","\\\"").replace(FAKE_SEP, "\"")
-        return out
+        return Command(*realCommand, path = repo).execute()
     }
 
-    fun add(fileName: String) = Command("$GIT add $fileName").execute()
 
-    fun restore(fileName: String) = Command("$GIT restore $fileName").execute()
+    fun status() = git("status")
 
-    fun fetch() = Command("$GIT fetch").execute()
+    fun fetch() = git("fetch")
+
+    fun branches() = git("branch","--all","-vv")
+
+    fun selectBranch(branch: String) = git("checkout",branch)
+
+    fun add(fileName: String) = git("add", fileName)
+
+    fun restore(fileName: String) = git("restore", fileName)
+
+    fun log() = git("--no-pager", "log", "--pretty=format:%H %P")
+
+    fun show(commit:Commit) = show(commit.hash)
+    fun show(commitHash:String) = git("--no-pager", "show" ,"-s", commitHash)
+
+
 }
