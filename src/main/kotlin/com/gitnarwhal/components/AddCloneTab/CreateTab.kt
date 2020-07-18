2b73c596c5b36c0bfb2a066c6b925c44977b453c
Fede Capece <capece.1744999@studenti.uniroma1.it>
2020-07-18 12:59:49 +0200

    Added functionality for the AddTab

--- src/main/kotlin/com/gitnarwhal/components/AddCloneTab/CreateTab.kt
+++ src/main/kotlin/com/gitnarwhal/components/AddCloneTab/CreateTab.kt
@@ -1,15 +1,14 @@
 package com.gitnarwhal.components.AddCloneTab
 
-import com.gitnarwhal.backend.Commit
-import com.gitnarwhal.backend.Git
 import com.gitnarwhal.views.AddCloneTab
 import javafx.scene.Parent
-import javafx.scene.control.Tab
-import javafx.scene.control.TableView
 import tornadofx.*
-import java.nio.file.Paths
-import kotlin.collections.HashMap
 
-class CreateTab: Fragment() {
+class CreateTab(private val addCloneTab: AddCloneTab) : Fragment() {
     override val root:Parent by fxml(null as String?, true)
+
+    fun run() {
+
+    }
+
 }
