49cfc77e75174a67b3dd7d35ae00c9f6c0506a79
Celeste Capece <celedev97@gmail.com>
2020-07-18 13:29:27 +0200

    now tabs can't be opened multiple times

--- src/main/kotlin/com/gitnarwhal/components/AddCloneTab/AddTab.kt
+++ src/main/kotlin/com/gitnarwhal/components/AddCloneTab/AddTab.kt
@@ -1,5 +1,6 @@
 package com.gitnarwhal.components.AddCloneTab
 
+import com.gitnarwhal.utils.Settings
 import com.gitnarwhal.utils.toPath
 import com.gitnarwhal.views.AddCloneTab
 import com.gitnarwhal.views.RepoTab
@@ -7,6 +8,7 @@ import javafx.scene.Parent
 import javafx.scene.control.Alert
 import javafx.scene.control.TextField
 import javafx.stage.DirectoryChooser
+import org.json.JSONObject
 import tornadofx.*
 import java.nio.file.Files
 
@@ -21,6 +23,13 @@ class AddTab(private val addCloneTab: AddCloneTab) : Fragment() {
     fun run() {
         if(!Files.isDirectory(path.text.toPath())){
             Alert(Alert.AlertType.ERROR,"Error: The specified path is not a directory").show()
+            return
+        }
+
+        var openTabsPaths = Settings.openTabs.map { (it as JSONObject).getString("path") }
+        if(openTabsPaths.contains(path.text)){
+            //TODO: handle this in a better way, like... switch the tab :v
+            Alert(Alert.AlertType.ERROR,"This repository is already open in another tab").showAndWait()
             return
         }
 
